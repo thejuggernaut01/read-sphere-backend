b2f30923c8eaacd8381a92c94d26ea70b3fcefb1
thejuggernaut01 <adewaleolanrewaju2002@gmail.com>
2024-09-18 22:15:55 +0100

    logger, database, validation configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,24 +1,62 @@
-import { Module } from '@nestjs/common';
+import { Module, ValidationPipe } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './components/user/user.module';
 import { SequelizeModule } from '@nestjs/sequelize';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { APP_PIPE } from '@nestjs/core';
+import { LoggerModule } from 'nestjs-pino';
 
 @Module({
   imports: [
-    UserModule,
-    SequelizeModule.forRoot({
-      dialect: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'pass',
-      database: 'store',
-      autoLoadModels: true, // Automatically load all models registered with SequelizeModule.forFeature
-      synchronize: true,
+    // dotenv configuration
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV}` as string,
     }),
+
+    // Logger configuration using pino
+    LoggerModule.forRoot({
+      pinoHttp: {
+        customProps: () => ({
+          context: 'HTTP',
+        }),
+        transport: {
+          target: 'pino-pretty',
+          options: {
+            singleLine: true,
+          },
+        },
+      },
+    }),
+
+    // Database configuration
+    SequelizeModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        return {
+          dialect: 'postgres',
+          host: 'localhost',
+          port: config.get<number>('DB_PORT'),
+          username: config.get<string>('DB_USERNAME'),
+          password: config.get<string>('DB_PASSWORD'),
+          database: config.get<string>('DB_NAME'),
+          autoLoadModels: true, // Automatically load all models registered with SequelizeModule.forFeature
+          synchronize: true,
+        };
+      },
+    }),
+    UserModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_PIPE,
+      useValue: new ValidationPipe({
+        whitelist: true,
+      }),
+    },
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger, LoggerErrorInterceptor } from 'nestjs-pino';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: true,
+    bufferLogs: true,
+  });
+
+  // configure logger
+  app.useLogger(app.get(Logger));
+  // Expose all error including error message, stack trace, etc.
+  app.useGlobalInterceptors(new LoggerErrorInterceptor());
   await app.listen(3000);
 }
 bootstrap();
