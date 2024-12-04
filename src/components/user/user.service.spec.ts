010c7225ed630d6ac79559147d4930ce82988f02
thejuggernaut01 <adewaleolanrewaju2002@gmail.com>
2024-12-04 10:25:10 +0100

    chore: user service unit test

--- src/components/user/user.service.spec.ts
+++ src/components/user/user.service.spec.ts
@@ -1,12 +1,47 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserService } from './user.service';
+import { faker } from '@faker-js/faker/.';
+import { UserModel } from './model/user.model';
+import { getModelToken } from '@nestjs/sequelize';
+import {
+  InternalServerErrorException,
+  NotFoundException,
+} from '@nestjs/common';
+import { ERROR_CONSTANT } from '../../common/constants/error.constant';
+import { Op } from 'sequelize';
 
 describe('UserService', () => {
   let service: UserService;
+  let mockUserModel: any;
+
+  const mockTransaction = {
+    commit: jest.fn(),
+    rollback: jest.fn(),
+  };
 
   beforeEach(async () => {
+    mockUserModel = {
+      sequelize: {
+        transaction: jest.fn().mockResolvedValue(mockTransaction),
+      },
+      build: jest.fn().mockImplementation((dto) => ({
+        ...dto,
+        save: jest.fn().mockResolvedValue({
+          id: faker.database.mongodbObjectId(),
+          ...dto,
+        }),
+      })),
+      findOne: jest.fn(),
+      findByPk: jest.fn().mockImplementation((id) => ({
+        id,
+      })),
+    };
+
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService],
+      providers: [
+        UserService,
+        { provide: getModelToken(UserModel), useValue: mockUserModel },
+      ],
     }).compile();
 
     service = module.get<UserService>(UserService);
@@ -14,5 +49,221 @@ describe('UserService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should create a user successfully', async () => {
+    const createUserDto = {
+      firstName: faker.person.firstName(),
+      lastName: faker.person.lastName(),
+      email: faker.internet.email(),
+      password: faker.internet.password({ length: 7 }),
+      termsAcceptedAt: faker.date.future(),
+    };
+
+    const user = await service.createUser(createUserDto);
+
+    expect(user).toBeDefined();
+    expect(mockUserModel.build).toHaveBeenCalledWith(createUserDto);
+    expect(mockTransaction.commit).toHaveBeenCalled();
+    expect(user).toEqual(expect.objectContaining(createUserDto));
+  });
+
+  it('should not create user and rollback the transaction on error', async () => {
+    mockUserModel.build.mockImplementationOnce(() => {
+      throw new Error('Database error');
+    });
+
+    const createUserDto = {
+      firstName: faker.person.firstName(),
+      lastName: faker.person.lastName(),
+      email: faker.internet.email(),
+      password: faker.internet.password({ length: 7 }),
+      termsAcceptedAt: faker.date.future(),
+    };
+
+    await expect(service.createUser(createUserDto)).rejects.toThrow(
+      new InternalServerErrorException(ERROR_CONSTANT.AUTH.REGISTER_FAILED),
+    );
+
+    expect(mockTransaction.rollback).toHaveBeenCalled();
+  });
+
+  it('should find a user by ID', async () => {
+    const createUserDto = {
+      firstName: faker.person.firstName(),
+      lastName: faker.person.lastName(),
+      email: faker.internet.email(),
+      password: faker.internet.password({ length: 7 }),
+      termsAcceptedAt: faker.date.future(),
+    };
+
+    const user = await service.createUser(createUserDto);
+
+    mockUserModel.findByPk.mockResolvedValue({
+      id: user.id,
+      ...createUserDto,
+    });
+
+    const foundUser = await service.findUserById(user.id);
+
+    expect(foundUser).toBeDefined();
+    expect(foundUser).toEqual({
+      id: user.id,
+      ...createUserDto,
+    });
+
+    expect(mockUserModel.findByPk).toHaveBeenCalledWith(user.id);
+  });
+
+  it('should find a user by email', async () => {
+    const email = faker.internet.email();
+    const mockUser = {
+      id: faker.database.mongodbObjectId(),
+      firstName: faker.person.firstName(),
+      lastName: faker.person.lastName(),
+      email,
+      password: faker.internet.password({ length: 7 }),
+      termsAcceptedAt: faker.date.future(),
+    };
+    mockUserModel.findOne.mockResolvedValue(mockUser);
+
+    const foundUser = await service.findUserByEmail(email);
+
+    expect(foundUser).toBeDefined();
+    expect(foundUser).toEqual(mockUser);
+
+    expect(mockUserModel.findOne).toHaveBeenCalledWith({
+      where: { email },
+    });
+  });
+
+  it('should return null if no user is found by email', async () => {
+    mockUserModel.findOne.mockResolvedValue(null);
+    const email = faker.internet.email();
+    const foundUser = await service.findUserByEmail(email);
+    expect(foundUser).toBeNull();
+    expect(mockUserModel.findOne).toHaveBeenCalledWith({ where: { email } });
+  });
+
+  it('should throw an error if findOne fails', async () => {
+    mockUserModel.findOne.mockRejectedValue(new Error('Database error'));
+    const email = faker.internet.email();
+    await expect(service.findUserByEmail(email)).rejects.toThrow(
+      'Database error',
+    );
+    expect(mockUserModel.findOne).toHaveBeenCalledWith({ where: { email } });
+  });
+
+  it('should update user reset token and expiration', async () => {
+    const token = faker.string.alphanumeric(20);
+    const password = faker.internet.password({ length: 7 });
+    const mockUser = {
+      resetPasswordToken: token,
+      resetPasswordTokenExpiration: new Date(Date.now() + 3600000),
+      update: jest.fn(),
+    };
+    mockUserModel.findOne.mockResolvedValue(mockUser);
+
+    const updatedUser = await service.findAndUpdateUserByResetToken(
+      token,
+      password,
+    );
+
+    expect(updatedUser).toBeDefined();
+    expect(updatedUser).toEqual(mockUser);
+    expect(mockUserModel.findOne).toHaveBeenCalledWith({
+      where: {
+        resetPasswordToken: token,
+        resetPasswordTokenExpiration: {
+          [Op.gt]: expect.any(Date),
+        },
+      },
+    });
+    expect(mockUser.update).toHaveBeenCalledWith({
+      resetPasswordToken: null,
+      resetPasswordTokenExpiration: null,
+      password: password,
+    });
+  });
+
+  it('should throw NotFoundException if user is not found', async () => {
+    const token = faker.string.alphanumeric(20);
+    const password = faker.internet.password({ length: 7 });
+    mockUserModel.findOne.mockResolvedValue(null);
+
+    await expect(
+      service.findAndUpdateUserByResetToken(token, password),
+    ).rejects.toThrow(new NotFoundException(ERROR_CONSTANT.GENERAL.TOKEN));
+
+    expect(mockUserModel.findOne).toHaveBeenCalledWith({
+      where: {
+        resetPasswordToken: token,
+        resetPasswordTokenExpiration: {
+          [Op.gt]: expect.any(Date),
+        },
+      },
+    });
+  });
+
+  it('should update user refresh token', async () => {
+    const email = faker.internet.email();
+    const token = faker.string.alphanumeric(20);
+
+    const mockUser = {
+      firstName: faker.person.firstName(),
+      lastName: faker.person.lastName(),
+      email,
+      password: faker.internet.password({ length: 7 }),
+      termsAcceptedAt: faker.date.future(),
+      update: jest.fn(),
+    };
+    mockUserModel.findOne.mockResolvedValue(mockUser);
+
+    const foundUser = await service.updateUserRefreshToken(email, token);
+
+    expect(foundUser).toBeDefined();
+    expect(foundUser).toEqual(mockUser);
+    expect(mockUserModel.findOne).toHaveBeenCalledWith({
+      where: { email },
+    });
+    expect(mockUser.update).toHaveBeenCalledWith({
+      refreshToken: token,
+    });
+  });
+
+  it('should throw NotFoundException if user is not found', async () => {
+    const email = faker.internet.email();
+    const token = faker.string.alphanumeric(20);
+    mockUserModel.findOne.mockResolvedValue(null);
+
+    await expect(service.updateUserRefreshToken(email, token)).rejects.toThrow(
+      new NotFoundException(ERROR_CONSTANT.AUTH.USER_DOES_NOT_EXIST),
+    );
+
+    expect(mockUserModel.findOne).toHaveBeenCalledWith({
+      where: { email },
+    });
+  });
+
+  it('should delete user', async () => {
+    const email = faker.internet.email();
+
+    const mockUser = {
+      firstName: faker.person.firstName(),
+      lastName: faker.person.lastName(),
+      email,
+      password: faker.internet.password({ length: 7 }),
+      termsAcceptedAt: faker.date.future(),
+      destroy: jest.fn(),
+    };
+    mockUserModel.findOne.mockResolvedValue(mockUser);
+
+    await service.deleteUser(email);
+
+    expect(mockUserModel.findOne).toHaveBeenCalledWith({
+      where: { email },
+    });
+
+    expect(mockUser.destroy).toHaveBeenCalled();
   });
 });
