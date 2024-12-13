d02884c01b84b84e84aeed44a3d31c0c2d548f8c
thejuggernaut01 <adewaleolanrewaju2002@gmail.com>
2024-12-13 19:33:48 +0100

    fix: Nest can't resolve dependencies of the AuthGuard

--- src/components/book/book.module.ts
+++ src/components/book/book.module.ts
@@ -3,9 +3,10 @@ import { BookController } from './book.controller';
 import { BookService } from './book.service';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { BookModel } from './model/book.model';
+import { UserModule } from '../user/user.module';
 
 @Module({
-  imports: [SequelizeModule.forFeature([BookModel])],
+  imports: [SequelizeModule.forFeature([BookModel]), UserModule],
   controllers: [BookController],
   providers: [BookService],
 })
