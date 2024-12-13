1c7e88ddb07dbd4b034d376473629d53d3677587
thejuggernaut01 <adewaleolanrewaju2002@gmail.com>
2024-12-13 19:27:53 +0100

    fix: Nest can't resolve dependencies of the BookService

--- src/components/book/book.module.ts
+++ src/components/book/book.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { BookController } from './book.controller';
 import { BookService } from './book.service';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { BookModel } from './model/book.model';
 
 @Module({
+  imports: [SequelizeModule.forFeature([BookModel])],
   controllers: [BookController],
   providers: [BookService],
 })
