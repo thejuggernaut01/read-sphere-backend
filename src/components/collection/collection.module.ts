7ec74b2eca8ff20a0940b411366337443451d520
thejuggernaut01 <adewaleolanrewaju2002@gmail.com>
2024-12-20 01:11:11 +0100

    chore: add book model to collection DI container

--- src/components/collection/collection.module.ts
+++ src/components/collection/collection.module.ts
@@ -4,10 +4,13 @@ import { CollectionController } from './collection.controller';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { CollectionModel } from './model/collection.model';
 import { UserModule } from '../user/user.module';
-import { CollectionBooksModel } from './model/collection-books.model';
+import { BookModel } from '../book/model/book.model';
 
 @Module({
-  imports: [SequelizeModule.forFeature([CollectionModel]), UserModule],
+  imports: [
+    SequelizeModule.forFeature([CollectionModel, BookModel]),
+    UserModule,
+  ],
   controllers: [CollectionController],
   providers: [CollectionService],
 })
